2457c59697f494515b579a70ed877f647c68aaf1
MOYE <56973455+lasertsole@users.noreply.github.com>
2025-12-08 10:58:24 +0800

    Update game_rule.rs

--- backend/src/game/game_rule.rs
+++ backend/src/game/game_rule.rs
@@ -7,30 +7,30 @@ use std::fmt; // 引入 fmt 模块
 
 /// 游戏规则
 pub struct GameRule {
-    pub compare:&'static dyn Fn(&Vec<&dyn GameItem>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>) -> bool,
-    pub allocate:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>) -> (),
-    pub game_start:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-    pub game_progress:&'static  dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-    pub game_pause:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-    pub game_resume:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-    pub game_finish:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-    pub game_wait_start:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-    pub players_join: &'static dyn Fn(&Vec<&Player>, &Vec<&Player>, &Vec<&dyn GameItem>, GameState, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-    pub players_leave: &'static dyn Fn(&Vec<&Player>, &Vec<&Player>, &Vec<&dyn GameItem>, GameState, &HashMap<String, Box<dyn Any + Send + Sync>>)->()
+    pub compare:Box<dyn Fn(&Vec<&dyn GameItem>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>) -> bool>,
+    pub allocate:Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>) -> ()>,
+    pub game_start:Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+    pub game_progress:Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+    pub game_pause:Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+    pub game_resume:Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+    pub game_finish:Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+    pub game_wait_start:Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+    pub players_join: Box<dyn Fn(&Vec<&Player>, &Vec<&Player>, &Vec<&dyn GameItem>, GameState, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+    pub players_leave: Box<dyn Fn(&Vec<&Player>, &Vec<&Player>, &Vec<&dyn GameItem>, GameState, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>
 }
 
 impl GameRule {
     pub fn new(
-        compare:&'static dyn Fn(&Vec<&dyn GameItem>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>) -> bool,
-        allocate:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>) -> (),
-        game_start:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-        game_progress:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-        game_pause:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-        game_resume:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-        game_finish:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-        game_wait_start:&'static dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-        players_join: &'static dyn Fn(&Vec<&Player>, &Vec<&Player>, &Vec<&dyn GameItem>, GameState, &HashMap<String, Box<dyn Any + Send + Sync>>)->(),
-        players_leave: &'static dyn Fn(&Vec<&Player>, &Vec<&Player>, &Vec<&dyn GameItem>, GameState, &HashMap<String, Box<dyn Any + Send + Sync>>)->()
+        compare: Box<dyn Fn(&Vec<&dyn GameItem>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>) -> bool>,
+        allocate: Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>) -> ()>,
+        game_start: Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+        game_progress: Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+        game_pause: Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+        game_resume: Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+        game_finish: Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+        game_wait_start:Box<dyn Fn(&Vec<&Player>, &Vec<&dyn GameItem>, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+        players_join: Box<dyn Fn(&Vec<&Player>, &Vec<&Player>, &Vec<&dyn GameItem>, GameState, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>,
+        players_leave: Box<dyn Fn(&Vec<&Player>, &Vec<&Player>, &Vec<&dyn GameItem>, GameState, &HashMap<String, Box<dyn Any + Send + Sync>>)->()>
     ) -> Self{
         GameRule {
             compare,
@@ -51,16 +51,16 @@ impl GameRule {
 impl fmt::Debug for GameRule {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("GameRule")
-            .field("compare", &"[Fn compare]")
-            .field("allocate", &"[Fn allocate]")
-            .field("game_start", &"[Fn game_start]")
-            .field("game_progress", &"[Fn game_progress]")
-            .field("game_pause", &"[Fn game_pause]")
-            .field("game_resume", &"[Fn game_resume]")
-            .field("game_finish", &"[Fn game_finish]")
-            .field("game_wait_start", &"[Fn game_wait_start]")
-            .field("players_join", &"[Fn players_join]")
-            .field("players_leave", &"[Fn players_leave]")
+            .field("compare", &"Box[Fn compare]")
+            .field("allocate", &"Box[Fn allocate]")
+            .field("game_start", &"Box[Fn game_start]")
+            .field("game_progress", &"Box[Fn game_progress]")
+            .field("game_pause", &"Box[Fn game_pause]")
+            .field("game_resume", &"Box[Fn game_resume]")
+            .field("game_finish", &"Box[Fn game_finish]")
+            .field("game_wait_start", &"Box[Fn game_wait_start]")
+            .field("players_join", &"Box[Fn players_join]")
+            .field("players_leave", &"Box[Fn players_leave]")
             .finish()
     }
 }
